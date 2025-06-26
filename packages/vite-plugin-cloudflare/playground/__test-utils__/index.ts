249bd1c1c135d44d316d97fa0c476a1e00272aef
Brett Beutell <brbeut@gmail.com>
2025-06-26 18:14:01 +0200

    Remove the playground

--- packages/vite-plugin-cloudflare/playground/__test-utils__/index.ts
+++
@@ -1,8 +0,0 @@
-import { test } from "vitest";
-
-export * from "../vitest-setup";
-export * from "./responses";
-
-export function failsIf(condition: boolean) {
-	return condition ? test.fails : test;
-}
--- packages/vite-plugin-cloudflare/playground/block-concurrency-while/__tests__/block-concurrency-while.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-import { getJsonResponse } from "../../__test-utils__";
-
-test("blocks execution until `ctx.blockConcurrencyWhile` has completed", async () => {
-	const response = await getJsonResponse("/durable-object");
-	expect(response).toEqual({ isInitialized: true });
-});
